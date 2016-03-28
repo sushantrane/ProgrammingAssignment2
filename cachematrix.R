04cdea32e324ace671af423b4d4137973706a510
Sushant Rane <rane.s@hotmail.com>
2016-03-27 19:36:29 -0500

    Adding assignment code

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,43 @@
 
 ## Write a short comment describing this function
 
+
+# makeCacheMatrix creates a list containing a function to get-set value of the matrix and get-set value of inverse of the matrix
+
 makeCacheMatrix <- function(x = matrix()) {
 
+  inv = NULL
+  set = function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
 
+#cacheSolve function returns the inverse of the matrix by checking cached value initially, if not found it computes the inverse
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inv = x$getinv()
+  
+  # Check if the inverse has already been calculated
+  if (!is.null(inv)){
+    
+    return(inv)
+  }
+  
+  # else calculate the inverse 
+  
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  x$setinv(inv)
+  
+  return(inv)
 }
